e09b917d97b5471b413c828ef07099d6d3a5f402
赵煜 <3198532542@qq.com>
2026-01-14 18:12:21 +0800

    test: switch to cloudflare tunnel

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -28,7 +28,7 @@ interface ChatMessage {
 }
 
 // 使用API前缀，将请求通过Node服务转发
-export const API_BASE_URL = "https://generations-none-nice-harold.trycloudflare.com";
+export const API_BASE_URL = "https://ability-higher-prove-reflection.trycloudflare.com";
 
 /**
  * 语音助手组件
